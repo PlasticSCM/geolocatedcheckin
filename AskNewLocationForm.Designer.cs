8664576e223467e26adc8a5740b4310413882379
pablo <>
2016-08-22 23:27:29 +0200

    Removed an unused handler

--- AskNewLocationForm.Designer.cs
+++ AskNewLocationForm.Designer.cs
@@ -124,7 +124,6 @@
             this.label3.Size = new System.Drawing.Size(61, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Longitude";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
